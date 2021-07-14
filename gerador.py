7a522507279bd9a041be39b02727e676d559570f
Particular <alexandresimoesf@gmail.com>
2021-07-13 21:57:02 -0300

    fix

--- gerador.py
+++ gerador.py
@@ -6,8 +6,9 @@ with open('pacientes_dermacapelli.csv', 'r', encoding='utf-8') as f:
     linhas = (list(csv.reader(f)))
     colunas = '{}, ' * (len(linhas[0]) - 1) + '{}'
     sql = 'INSERT INTO public.paciente (' + colunas.format(*linhas[0]) + ') VALUES (' + colunas + ');'
-    for i in linhas[1:5]:
-        print(sql.format(*i))
+    with open('paciente.sql', "a") as arquivo_sql:
+        for i in linhas[1:]:
+            print(sql.format(*i), file=arquivo_sql)
 
 
 
