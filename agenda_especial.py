c091bfaacced2df42a5e755ecd33ab5da7a886bf
Particular <alexandresimoesf@gmail.com>
2021-07-17 15:20:50 -0300

    gerador.py

--- agenda_especial.py
+++ agenda_especial.py
@@ -1,9 +1,15 @@
-from pandas import read_csv, set_option
+from pandas import read_csv, set_option, DataFrame
 
+# select * from medico where id in (203, 210, 211, 212)
 
 set_option('display.max_rows', 500)
 set_option('display.max_columns', 500)
 set_option('display.width', 2000)
+
+
+
+def prontuario_paciente_fk(key):
+    return '(SELECT id FROM public.paciente WHERE paciente.id_paciente_dermacapelli = {})'.format(key)
 
 
 def sql_foreing_key_paciente(key):
@@ -22,7 +28,12 @@ def horario(h):
     return '08:00' if 'M' in h or 'T' in h else h
 
 
-agenda_do_medico = read_csv('LEONARDO.csv', sep=';', encoding='latin-1', low_memory=False)
+def prontuario(key):
+    return '(SELECT id FROM public.prontuario WHERE fk_paciente_id = (SELECT id FROM paciente where paciente.id_paciente_dermacapelli={} LIMIT 1))'.format(key)
+
+
+
+agenda_do_medico = read_csv('SOFIA.csv', sep=';', encoding='latin-1', low_memory=False)
 
 usando = ['Data', 'Hora', 'CodPaciente']
 n_usando = ['Data', 'Hora', 'NomePaciente', 'Atendente', 'PacienteConfirmado',
@@ -41,12 +52,12 @@ agenda_do_medico['status_consulta'] = "'AGUARDANDO'"
 agenda_do_medico['confirm_consulta'] = "'CONFIRMADO'"
 agenda_do_medico['codigo_saida'] = "'RETORNO'"
 agenda_do_medico['data_finalizacao'] = 'null'
-agenda_do_medico['responsavel_recepcao'] = ''
+agenda_do_medico['responsavel_recepcao'] = "''"
 agenda_do_medico['paciente_online'] = 'false'
 agenda_do_medico['fk_clinica_id'] = 83
-agenda_do_medico['fk_especializacao_id'] = 776
+agenda_do_medico['fk_especializacao_id'] = 798
 agenda_do_medico['fk_forma_atendimento_id'] = 230
-agenda_do_medico['fk_medico_id'] = 203
+agenda_do_medico['fk_medico_id'] = 210
 agenda_do_medico = agenda_do_medico.dropna(subset=['Data', 'CodPaciente'])
 agenda_do_medico['Data'] = agenda_do_medico['Data'].astype(str).apply(formatar_data)
 agenda_do_medico['CodPaciente'] = agenda_do_medico['CodPaciente'].astype(int)
@@ -59,5 +70,28 @@ agenda_do_medico = agenda_do_medico.drop(['Data', 'Hora'], axis=1)
 agenda_do_medico = agenda_do_medico.rename(columns={'CodPaciente': 'fk_paciente_id'})
 agenda_do_medico['fk_paciente_id'] = agenda_do_medico['fk_paciente_id'].apply(sql_foreing_key_paciente)
 agenda_do_medico[['horario', 'data_agendada', 'data_agendada_timestamp', 'data_solicitacao']] = agenda_do_medico[['horario', 'data_agendada', 'data_agendada_timestamp', 'data_solicitacao']].apply(quote)
-agenda_do_medico.to_csv('csv/leonardo_agenda.csv', index=False)
+agenda_do_medico.to_csv('csv/sofia_agenda_dermacapelli.csv', index=False)
 
+
+prontuario_csv = DataFrame()
+prontuario_csv['fk_paciente_id'] = agenda_do_medico['fk_paciente_id'].values
+prontuario_csv['fk_paciente_id'] = prontuario_csv['fk_paciente_id'].apply(prontuario_paciente_fk)
+prontuario_csv['datacriacao'] = '(SELECT now())'
+prontuario_csv = prontuario_csv.drop_duplicates(subset=['fk_paciente_id'])
+prontuario_csv.to_csv('csv/sofia_prontuario_dermacapelli.csv', encoding='UTF8', index=False)
+
+
+prontuarioPermissao_csv = DataFrame()
+prontuarioPermissao_csv['id_paciente_dermacapelli'] = agenda_do_medico['fk_paciente_id']
+prontuarioPermissao_csv['modificado_em'] = '(SELECT now())'
+prontuarioPermissao_csv['fk_medico_id'] = agenda_do_medico['fk_medico_id'].values
+
+prontuarioPermissao_csv['fk_prontuario_id'] = agenda_do_medico['fk_paciente_id'].apply(prontuario)
+prontuarioPermissao_csv['fk_rede_clinica_id'] = '(SELECT fk_rede_clinica_id FROM public.clinica WHERE id = (83))'
+prontuarioPermissao_csv['escrita'] = 'false'
+prontuarioPermissao_csv['leitura'] = 'false'
+
+prontuarioPermissao_csv = prontuarioPermissao_csv.groupby(['id_paciente_dermacapelli', 'fk_medico_id']).first()
+prontuarioPermissao_csv = prontuarioPermissao_csv.reset_index()
+prontuarioPermissao_csv = prontuarioPermissao_csv.drop(['id_paciente_dermacapelli'], axis=1)
+prontuarioPermissao_csv.to_csv('csv/sofia_prontuarioPermissao_dermacapelli.csv', encoding='UTF8', index=False)
--- gerador.py
+++ gerador.py
@@ -4,10 +4,14 @@ import os
 
 def gerar_sql():
     for arq in os.listdir('csv'):
-        tabela = arq.split('_')[0]
+        if len(arq.split('_')) > 2:
+            tabela = arq.split('_')[1]
+            arquivo_final = 'sql/' + tabela + '_' +arq.split('_')[0] + '.sql'
+        else:
+            tabela = arq.split('_')[0]
+            arquivo_final = 'sql/' + tabela + '.sql'
         if tabela == 'prontuarioPermissao':
             tabela = 'permissao_prontuario_clinica'
-        arquivo_final = 'sql/' + tabela + '.sql'
         with open('csv/' + arq, 'r', encoding='utf-8') as f:
             linhas = (list(csv.reader(f)))
             colunas = '{}, ' * (len(linhas[0]) - 1) + '{}'
@@ -15,3 +19,5 @@ def gerar_sql():
             with open(arquivo_final, "a", encoding='utf-8') as arquivo_sql:
                 for i in linhas[1:]:
                     print(sql.format(*i), file=arquivo_sql)
+
+# gerar_sql()
